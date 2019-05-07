f9e11907eca19ff802347fc54df96987f2108331
Kuba <jakzacha@gmail.com>
2019-05-07 22:53:30 +0200

    Updates

---
+++ precache-manifest.89f248bb6c38c8530d787409c396c368.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "94e78e241d2b8678201b",
+    "url": "/MemoryGame/static/css/main.cb4d6cb2.chunk.css"
+  },
+  {
+    "revision": "94e78e241d2b8678201b",
+    "url": "/MemoryGame/static/js/main.3343c149.chunk.js"
+  },
+  {
+    "revision": "4c3bd79ed0e125202499",
+    "url": "/MemoryGame/static/js/runtime~main.d54f135b.js"
+  },
+  {
+    "revision": "ad05d52b867dad714ca4",
+    "url": "/MemoryGame/static/js/2.d5e98253.chunk.js"
+  },
+  {
+    "revision": "ae87ad37307ce14dde6758f44ac8c657",
+    "url": "/MemoryGame/static/media/background3.ae87ad37.svg"
+  },
+  {
+    "revision": "83eaf017c56e956710da298c7a672a7e",
+    "url": "/MemoryGame/index.html"
+  }
+];
